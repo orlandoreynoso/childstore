2acf448606b2920519a6d02206900924ca684415
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-11-09 09:02:48 -0600

    start template mas por tus ...things

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* devuelve el directorio de stilo de css, y se le asigna a temppath */
 define('TEMPPATH',get_bloginfo('stylesheet_directory'));
 /*En esto concatenamos la carpeta images*/
@@ -10,7 +10,7 @@ add_theme_support('post-thumbnails');
 /*========== incluyendo los estilo que uso ===============*/
 //include (TEMPLATEPATH. '/inc/estilos.php');
 //add_image_size( $name, $width, $height, $crop );
-the_post_thumbnail('thumbnail',1024,1024,true);       // Tamaño de la miniatura 150x150 píxeles 
+the_post_thumbnail('thumbnail',1024,1024,true);       // Tamaño de la miniatura 150x150 píxeles
 the_post_thumbnail('medium',300,300, true);          // Tamaño de la mediano 300x300 píxeles
 the_post_thumbnail('large');
 the_post_thumbnail('pastorales', 751, 422, true);
@@ -37,7 +37,7 @@ wp_register_style('estilo', get_stylesheet_directory_uri().'/css/estilo.css', ar
 
 	// Registrar los estilos
 	wp_register_style('fonts_google', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i|Oswald:400,700', array(), '1.0.0');
-	wp_register_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css', array(), '4.0.0');
+	// wp_register_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css', array(), '4.0.0');
 	wp_register_style('style', get_stylesheet_directory_uri().'/style.css', array(), '1.0');
 	wp_register_style('estilo', get_stylesheet_directory_uri().'/css/estilo.css', array(), '7.0');
   wp_register_style('animate', get_stylesheet_directory_uri().'/css/animate.css', array(), '7.0');
@@ -47,7 +47,7 @@ wp_register_style('estilo', get_stylesheet_directory_uri().'/css/estilo.css', ar
 
 	//Llamar a los estilos
 	wp_enqueue_style('style');
-	wp_enqueue_style('bootstrap');
+	// wp_enqueue_style('bootstrap');
 	wp_enqueue_style('estilo');
   wp_enqueue_style('animate');
 	wp_enqueue_style('fonts_google' );
@@ -56,8 +56,8 @@ wp_register_style('estilo', get_stylesheet_directory_uri().'/css/estilo.css', ar
 
 	//registrar Js, agregamos al final true para que nuestros Js se cargen en el footer.
 	//wp_register_script('analitics', get_template_directory_uri().'/js/google.js', array(), '1.0.0', true );
-	wp_register_script('bootstrapjs', get_stylesheet_directory_uri().'/js/bootstrap.js', array('jquery'),'4.0.0', true);
-	wp_register_script('popper', get_stylesheet_directory_uri().'/js/popper.min.js', array('jquery'),'4.0.0', true);
+	// wp_register_script('bootstrapjs', get_stylesheet_directory_uri().'/js/bootstrap.js', array('jquery'),'4.0.0', true);
+	// wp_register_script('popper', get_stylesheet_directory_uri().'/js/popper.min.js', array('jquery'),'4.0.0', true);
   wp_register_script('waypoint', get_stylesheet_directory_uri().'/js/waypoint.js', array('jquery'),'4.0.0', true);
   wp_register_script('smoothscroll', get_stylesheet_directory_uri().'/js/smooth-scroll.js', array('jquery'),'4.0.0', true);
   wp_register_script('scripts', get_stylesheet_directory_uri().'/inc/scripts.js', array('jquery'),'1.0.0', true);
@@ -65,8 +65,8 @@ wp_register_style('estilo', get_stylesheet_directory_uri().'/css/estilo.css', ar
 
 	wp_enqueue_script('jquery');
 	//wp_enqueue_script('analitics');
-	wp_enqueue_script('popper');
-	wp_enqueue_script('bootstrapjs');
+	// wp_enqueue_script('popper');
+	// wp_enqueue_script('bootstrapjs');
 	wp_enqueue_script('smoothscroll');
   wp_enqueue_script('waypoint');
 
@@ -177,6 +177,6 @@ add_action( 'storefront_header', 'storefront_primary_navigation_wrapper',       
 			 * @hooked storefront_header_cart                      - 60
 			 * @hooked storefront_primary_navigation_wrapper_close - 68
 			 */
-			//do_action( 'storefront_header' ); 
+			//do_action( 'storefront_header' );
 
 ?>
